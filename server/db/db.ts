726dfa3b221a82430b66c8c9763c8364cbb034d6
Sarah Zhong <sarahchungwu@gmail.com>
2023-06-14 13:28:01 +1200

    added typescript

--- server/db/db.ts
+++ server/db/db.ts
@@ -2,10 +2,12 @@ import connection from './connection'
 import { ToDoDraft, ToDo } from '../../modules/todo'
 
 export async function getTodos(db = connection) {
-  return (await db('todos').select()) as ToDo[]
+  return (await db('todos').select()) as Promise<ToDo[]>
 }
 
-export async function addTodo(input: ToDoDraft, db = connection) {
-  console.log('I am working')
-  return await db('todos').insert(input)
+export async function addTodo(
+  input: ToDoDraft,
+  db = connection
+): Promise<void> {
+  await db('todos').insert(input)
 }
