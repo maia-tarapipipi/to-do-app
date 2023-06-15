c9325e3c181757bd01af70f92dc88bf2cd56f97b
Daphne Simons <daphnejasminesimons@gmail.com>
2023-06-15 12:00:08 +1200

    synced up migration and seed column names

--- server/db/seeds/tasks.js
+++ server/db/seeds/tasks.js
@@ -2,24 +2,23 @@ exports.seed = async function (knex) {
   // Deletes ALL existing entries
   await knex('todos').del()
   await knex('todos').insert([
-        {
-          id: 1,
-          name: 'Finish my Trellos',
-          priority: 'High',
-          completed: true
-        },
-        {
-          id: 2,
-          name: 'Laundry',
-          priority: 'Low',
-          completed: false
-        },
-        {
-          id: 3,
-          name: 'Exercise',
-          priority: 'High',
-          completed: true
-        },
-        
-      ])
-    }
+    {
+      id: 1,
+      task: 'Finish my Trellos',
+      priority: 'High',
+      completed: true,
+    },
+    {
+      id: 2,
+      task: 'Laundry',
+      priority: 'Low',
+      completed: false,
+    },
+    {
+      id: 3,
+      task: 'Exercise',
+      priority: 'High',
+      completed: true,
+    },
+  ])
+}
