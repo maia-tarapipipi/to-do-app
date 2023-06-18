6c22a9a6bcd02e5a1575a5b9f511dc41f9c999f3
Carlos Aguilar <aguilar.pinero@gmail.com>
2023-06-18 14:41:10 +1200

    process app deploying almost done

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,15 @@
-import server from './server'
+import server from './server';
+import dotenv from 'dotenv';
 
-const PORT = process.env.PORT || 3000
+// Load the .env file
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+  const envConfig = dotenv.config();
+  if (envConfig.error) throw envConfig.error;
+}
+
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log('Listening on port', PORT)
-})
+  console.log('Listening on port', PORT);
+});
