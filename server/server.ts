47520a6933913ef003956605f61cc3bdd74edc75
Carlos Aguilar <aguilar.pinero@gmail.com>
2023-06-18 16:47:08 +1200

    fixing deploying issues 8

--- server/server.ts
+++ server/server.ts
@@ -5,13 +5,13 @@ import todos from './routes/todos'
 const server = express()
 server.use(express.json())
 
+server.use('/api/v1/todos/', todos)
+
 if (process.env.NODE_ENV === 'production') {
   server.use('/assets', express.static(Path.resolve(__dirname, '../assets')))
   server.get('*', (req, res) => {
     res.sendFile(Path.resolve(__dirname, '../index.html'))
   })
 }
-
-server.use('/api/v1/todos/', todos)
 
 export default server
