16c67e6bb5461d46499393151852ace9f3b90baf
Rocio-Peralta <1rocioperalta@gmail.com>
2023-06-19 14:43:37 +1200

    change some files

--- knexfile.js
+++ knexfile.js
@@ -27,21 +27,11 @@ module.exports = {
       tableName: 'knex_migrations'
     }
   },
-
   production: {
-    client: 'postgresql',
+    client: 'sqlite3',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      filename: '/app/storage/dev.sqlite3',
     },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
+    useNullAsDefault: true,
+  },
 };
