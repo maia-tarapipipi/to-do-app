a290b39aa161fcff770ff5828a8d9269f10ddb60
Leroy Brown <leroybrown247@gmail.com>
2023-06-21 22:54:12 +1200

    Thunk & Redux actions working, Deployment process begun

--- server/index.ts
+++ server/index.ts
@@ -5,7 +5,7 @@ import routes from './routes/todos'
 const app = express()
 
 
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 3001;
 
 server.listen(PORT, () => {
   console.log('Listening on port', PORT);
