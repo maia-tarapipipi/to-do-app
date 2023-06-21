a33735d37459aedd232c05ce80975f57eced403a
Tem Pearse <tem.pearse01@gmail.com>
2023-06-21 19:18:37 +1200

    still trying getting index.ts not found

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,10 @@
 import server from './server'
 
+// Load the .env file
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+  const envConfig = require('dotenv').config()
+  if (envConfig.error) throw envConfig.error
+}
 const PORT = process.env.PORT || 3000
 
 server.listen(PORT, () => {
