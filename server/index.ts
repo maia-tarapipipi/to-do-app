9a29fe0f005029722c8f7e6f81a58e256d7e5603
leroy brown <leroybrown247@gmail.com>
2023-06-22 16:44:57 +1200

    Doing step 11 of Dokku checklist

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,11 @@
 import server from './server';
-import express from 'express'
-import routes from './routes/todos'
 
-const app = express()
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+  const envConfig = require('dotenv').config()
+  if (envConfig.error) throw envConfig.error
+}
 
-
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
   console.log('Listening on port', PORT);
