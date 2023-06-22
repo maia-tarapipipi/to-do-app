0af4833abf56b04a9bff78b3f2b98787c8bfe505
leroy brown <leroybrown247@gmail.com>
2023-06-22 22:57:16 +1200

    index.ts file updated

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,15 @@
 import server from './server';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 
+// Load the .env file
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-  const envConfig = dotenv.config()
-  if (envConfig.error) throw envConfig.error
+  const envConfig = dotenv.config();
+  if (envConfig.error) throw envConfig.error;
 }
 
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log('Listening on port', PORT);
 });
-
