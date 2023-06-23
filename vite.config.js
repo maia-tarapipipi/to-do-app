dd089344d32bed989c090329933993cc9faa8bc3
Jordan-Lowe <jordanlowe659@gmail.com>
2023-06-24 09:23:20 +1200

    Fixed localhost error

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': '/',
-    },
-  },
+plugins: [react()],
+server: {
+proxy: {
+'/api':
+process.env.NODE_ENV === 'production' ? '/' : 'http://localhost:3000',
+},
+},
 })
